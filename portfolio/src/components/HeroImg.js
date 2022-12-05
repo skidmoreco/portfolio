a42587694a7e91a66268c86654c46075686244b1
Corey Skidmore <skidmoreeco@gmail.com>
2022-12-05 11:45:18 -0500

    working navbar & hamburger

---
+++ portfolio/src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import "./HeroImgStyles.css";
+
+import IntroImg from "../assets/introbg.jpg";
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="intro-img" src={IntroImg} alt="IntroImg"/>
+    </div>
+    <div className="content">
+        <p>Hi! My Name's Corey.</p>
+        <h1>Welcome to my Portfolio!</h1>
+        <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+    </div>
+  </div>
+  );
+};
+
+export default HeroImg;
