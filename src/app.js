81fb938aeffab57a4f4656dfea226083ab6520ab
skidmoreco <106204668+skidmoreco@users.noreply.github.com>
2022-12-20 19:55:16 -0500

    adjusting routes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function App() {
     return (
         <>
         <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/portfolio" element={<Home />}/>
             <Route path="/project" element={<Project />}/>
             <Route path="/about" element={<About />}/>
             <Route path="/contact" element={<Contact />}/>
